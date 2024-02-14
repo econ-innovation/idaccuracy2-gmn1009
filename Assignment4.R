2e62fffe2949be3713c86bb7e2f42f4aa48c48ba
gmn1009@163.com <gmn1009@163.com>
2024-02-14 11:14:48 +0800

    assignment4

--- Assignment4.R
+++ Assignment4.R
@@ -11,23 +11,22 @@ toupper(journal))
 files <- list.files(path = "Aminer", pattern = "\\.csv$", full.names = 
 TRUE, recursive = TRUE)
 
-# 定义一个函数来处理每个文件并计算准确率和召回率
 process_file <- function(file_path) {
   aminer_data <- read_csv(file_path) %>%
     mutate(doi = toupper(doi), title = toupper(title), journal = 
 toupper(journal))
   
-  # 提取 uniqueID
+  # uniqueID
   unique_ID <- str_extract(file_path, "0_[0-9]+")
   
-  # 筛选scientist_pub中uniqueID对应的数据
+  
   scientist_pub_filtered <- filter(scientist_pub, uniqueID == unique_ID)
   
-  # 筛选匹配的论文数据
+  
   matched_papers <- inner_join(aminer_data, scientist_pub_filtered, by = 
 c("doi", "title", "journal"))
   
-  # 计算精准度和召回率
+  # 计算
   precision <- nrow(matched_papers) / nrow(aminer_data)
   recall <- nrow(matched_papers) / nrow(scientist_pub_filtered)
   
@@ -35,20 +34,14 @@ c("doi", "title", "journal"))
 recall))
 }
 
-# 应用函数到每个文件
 results <- lapply(files, process_file)
 
-# 合并结果
-final_results <- bind_rows(results)
+results <- bind_rows(results)
 
-# 保存结果到文件，文件保存在当前工作目录
-write_csv(final_results, "accuracy_recall_results.csv")
+write_csv(results, "results.csv")
 
-# 计算整体准确率和召回率
-overall_precision <- mean(final_results$precision)
-overall_recall <- mean(final_results$recall)
+precision <- mean(final_results$precision)
+recall <- mean(final_results$recall)
 
-# 打印整体准确率和召回率
-print(paste("Overall Precision: ", overall_precision))
-print(paste("Overall Recall: ", overall_recall))
+
 
